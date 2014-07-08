ce9395639c61f0f77559c9d29ec079c89cac0421
Reilly Tucker Siemens <reilly.siemens@gmail.com>
2014-07-08 16:29:19 -0700

    Remove unnecessary call to list as map already returns an iterable.

--- sci2int.py
+++ sci2int.py
@@ -7,5 +7,5 @@ newfile = open(str(sys.argv[2]), 'w', encoding='utf-8')
 
 for line in oldfile:
     sci_nums = line.lstrip(' ').rstrip('\n').split('  ')
-    new_nums = list(map(lambda x: int(float(x)), sci_nums))
+    new_nums = map(lambda x: int(float(x)), sci_nums)
     print(' '.join(['{}'.format(d) for d in new_nums]), file=newfile)
